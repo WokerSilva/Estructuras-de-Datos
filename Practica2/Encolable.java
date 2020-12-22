fd23edefe9041e80086935de8a149347bde589f9
WokerSilva <41647915+WokerSilva@users.noreply.github.com>
2020-12-21 19:03:17 -0600

    Create Encolable.java

---
+++ Practica2/Encolable.java
@@ -0,0 +1,31 @@
+import java.util.NoSuchElementException;
+
+/**
+ * <p> Interfaz para Colas. Esta clase contiene las
+ * operaciones elementales que debe tener el TAD Cola </p>
+ * @author Marco Silva
+ * @version 2.0
+ * @param <T>
+ */
+public interface Encolable<T> {
+
+    /**
+     * Método para encolar un elemento a la cola.
+     * @param elemento Objeto que se agregará a la cola.
+     */
+    public void queue(T elemento) throws IllegalArgumentException;
+
+    /**
+     * Método para desencolar un elemento.
+     * @return El elemento que fue desencolado
+     */
+    public T dequeue() throws NoSuchElementException;
+    
+    /**
+     * Método para mirar lo que hay al inicio de la Cola
+     * @return El elemento que esta al inicio de la cola
+     */
+    public T peek() throws NoSuchElementException;
+
+
+}
